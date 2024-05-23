361a852e8671673aabed198e1f30d4e4f3085726
Ben <benmbrowning@gmail.com>
2024-05-22 21:40:19 -0400

    js fix

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
+var count = 0;
 const body = document.querySelector("body")
 if (body === null) {
     console.log("no body");
 }
 
-const win_height = window.innerHeight
+var win_height = window.innerHeight
 
-function set_height(element, height) {
-    element.style.height = height + "px"
+function set_height() {
+    body.style.height = win_height + "px";
+    console.log(count);
+    count = count + 1;
 }
 
-window.addEventListener("resize", set_height(body, win_height))
-set_height(body, win_height)
+window.addEventListener("resize", set_height());
+window.onresize = set_height();
+set_height(body, win_height);
 
-console.log("done")
+console.log("done");
