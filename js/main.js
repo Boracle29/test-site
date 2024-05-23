51c964d50957eec9f47bd7f91ce0a1dd2702eefe
Ben <benmbrowning@gmail.com>
2024-05-22 21:29:52 -0400

    style

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
-const body = document.body
+const body = document.querySelector("body")
+if (body === null) {
+    console.log("no body");
+}
 
-body.style.height = (window.innerHeight)
+const win_height = window.innerHeight
+
+function set_height(element, height) {
+    element.style.height = height + "px"
+}
+
+window.addEventListener("resize", set_height(body, win_height))
+set_height(body, win_height)
 
 console.log("done")
